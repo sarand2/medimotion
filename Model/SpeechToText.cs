970b40b8414c96286cb9d319b6d9b9e943cc9a95
kazimierasktu <kazimieras.buskus@ktu.edu>
2019-12-17 00:44:42 +0200

    Voice recognition

---
+++ Model/SpeechToText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediMotion.Model
+{
+    public class SpeechToText
+    {
+    }
+}
